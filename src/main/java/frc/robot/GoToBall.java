32e5adea24c7534167ac6d4267927d2433f31fbe
wblrobotics <wblrobotics@gmail.com>
2026-03-20 15:09:39 -0500

    changed ip for real bot driving

--- src/main/java/frc/robot/GoToBall.java
+++ src/main/java/frc/robot/GoToBall.java
@@ -32,7 +32,7 @@ public class GoToBall {
     public static void main(String[] args) {
         NetworkTableInstance inst2 = NetworkTableInstance.getDefault();
         inst2.startClient4("GoToBallViewer");          // set a client identity name
-        inst2.setServer("localhost", 5810);            // simulator runs NT on port 5810
+        inst2.setServer("10.22.7.2", 5810);            // simulator runs NT on port 5810
         
         // Attempt to pre-load the ntcorejni native library from the project's build folder so
         // direct 'java frc.robot.GoToBall' runs (or IDE runs) can find the JNI without requiring
